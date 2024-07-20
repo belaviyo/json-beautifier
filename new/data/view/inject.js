85b06d720d44f21893f74c5425712c2097a43bd2
Joe Ertaba <belaviyo@gmail.com>
2024-07-20 07:10:59 -0400

    Consistent formatting of the downloaded JSON

--- new/data/view/inject.js
+++ new/data/view/inject.js
@@ -82,7 +82,9 @@ buttons.save = {
     icon: [48, 48, [], '', `M34 6H10c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4V14l-8-8zM24 38c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm6-20H10v-8h20v8z`]
   },
   onClick() {
-    const content = editor.get().text;
+    const o = editor.get();
+    const json = o.json || LosslessJSON.parse(o.text);
+    const content = LosslessJSON.stringify(json, null, '  ');
 
     const blob = new Blob([content], {
       type: 'application/json;charset=utf-8'
