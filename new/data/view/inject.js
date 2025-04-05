888b2ff2676667aeff26e5c96559a9b133dca727
Joe Ertaba <belaviyo@gmail.com>
2025-04-05 03:44:06 -0700

    indentation size based on #27

--- new/data/view/inject.js
+++ new/data/view/inject.js
@@ -100,6 +100,53 @@ buttons.save = {
   title: 'Save JSON to the default Download Directory',
   type: 'button'
 };
+// headers
+buttons.headers = {
+  className: 'jse-headers',
+  disabled: false,
+  icon: {
+    prefix: 'fas',
+    iconName: 'jse-headers',
+    icon: [384, 512, [], '',
+      'M64 464c-8.8 0-16-7.2-16-16L48 64c0-8.8 7.2-16 16-16l160 0 0 80c0 17.7 14.3 32 32 32l80 0 0 288c0 8.8-7.2 16-16 16L64 464zM64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-293.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0L64 0zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24l144 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-144 0z'
+    ]
+  },
+  async onClick(e) {
+    this.disabled = true;
+    try {
+      const r = await fetch(location.href);
+      const div = document.createElement('div');
+      div.classList.add('headers');
+      const c = document.createElement('div');
+      div.append(c);
+      for (const [key, value] of r.headers) {
+        const a = document.createElement('span');
+        a.classList.add('key');
+        a.textContent = key;
+        const b = document.createElement('span');
+        b.classList.add('value');
+        b.textContent = value;
+
+        c.append(a, b);
+      }
+      div.setAttribute('popover', 'auto');
+      (document.body || document.documentElement).append(div);
+      div.addEventListener('toggle', e => {
+        if (e.newState === 'closed') {
+          div.remove();
+        }
+      });
+      div.showPopover();
+    }
+    catch (e) {
+      console.error(e);
+      alert(e.message);
+    }
+    this.disabled = false;
+  },
+  title: 'View Server Headers',
+  type: 'button'
+};
 
 async function render() {
   // https://github.com/belaviyo/json-beautifier/issues/16
@@ -118,7 +165,8 @@ async function render() {
   const prefs = await new Promise(resolve => chrome.storage.local.get({
     'mode': 'tree',
     'expandLevel': 2,
-    'auto-format': true
+    'auto-format': true,
+    'indentation': 2
   }, resolve));
   if (prefs.mode === 'code') { // backward compatibility
     prefs.mode = 'text';
@@ -133,11 +181,20 @@ async function render() {
   theme();
 
   try {
-    const {createJSONEditor} = await import(chrome.runtime.getURL('/data/view/json-editor/standalone.js'));
+    document.addEventListener('securitypolicyviolation', e => {
+      if (e.violatedDirective === 'style-src-elem' && e.sourceFile?.includes('extension://')) {
+        chrome.runtime.sendMessage({
+          method: 'insert-css',
+          css: e.target.textContent
+        });
+      }
+    });
 
+    const {createJSONEditor} = await import(chrome.runtime.getURL('/data/view/json-editor/standalone.js'));
     const props = {
       mode: prefs.mode,
       parser: LosslessJSON,
+      indentation: prefs.indentation,
       content: {},
       askToFormat: true,
       onRenderMenu(items, context) {
@@ -151,7 +208,7 @@ async function render() {
 
         items.push({
           type: 'separator'
-        }, buttons.refresh, buttons.save);
+        }, buttons.headers, buttons.refresh, buttons.save);
         return items;
       },
       onSelect(selection) {
@@ -196,6 +253,7 @@ async function render() {
   }
   catch (e) {
     console.error('[JSON Editor]', e);
+
     document.documentElement.classList.remove('jsb');
     if (location.protocol.startsWith('http')) {
       chrome.runtime.sendMessage({
--- new/worker.js
+++ new/worker.js
@@ -34,6 +34,23 @@ const onMessage = (request, {tab}, response) => {
   else if (request.method === 'get-json') {
     response(cache[tab.id]);
   }
+  else if (request.method === 'insert-css') {
+    const target = {
+      tabId: tab.id
+    };
+    if (location.protocol.startsWith('safari')) {
+      chrome.scripting.insertCSS({
+        target,
+        files: [`data:text/css;base64,${btoa(request.css)}`]
+      });
+    }
+    else {
+      chrome.scripting.insertCSS({
+        target,
+        css: request.css
+      });
+    }
+  }
 };
 chrome.runtime.onMessage.addListener(onMessage);
 
@@ -75,11 +92,16 @@ chrome.action.onClicked.addListener(() => chrome.tabs.create({
         type: 'checkbox',
         checked: prefs['auto-format']
       }, () => chrome.runtime.lastError);
-
+      chrome.contextMenus.create({
+        title: 'Configure',
+        contexts: ['action'],
+        id: 'configure'
+      }, () => chrome.runtime.lastError);
       chrome.contextMenus.create({
         title: 'Theme',
         contexts: ['action'],
-        id: 'theme'
+        id: 'theme',
+        parentId: 'configure'
       }, () => chrome.runtime.lastError);
       chrome.contextMenus.create({
         title: 'Light',
@@ -108,7 +130,8 @@ chrome.action.onClicked.addListener(() => chrome.tabs.create({
       chrome.contextMenus.create({
         title: 'Expand Level',
         contexts: ['action'],
-        id: 'expandLevel'
+        id: 'expandLevel',
+        parentId: 'configure'
       }, () => chrome.runtime.lastError);
       chrome.contextMenus.create({
         title: 'Level 0',
@@ -159,10 +182,48 @@ chrome.action.onClicked.addListener(() => chrome.tabs.create({
         id: 'expandLevel:-1',
         parentId: 'expandLevel'
       }, () => chrome.runtime.lastError);
+      chrome.contextMenus.create({
+        title: 'Indentation',
+        contexts: ['action'],
+        id: 'indentation',
+        parentId: 'configure'
+      }, () => chrome.runtime.lastError);
+      chrome.contextMenus.create({
+        title: 'Use Tab',
+        contexts: ['action'],
+        id: 'indentation:\t',
+        type: 'radio',
+        parentId: 'indentation'
+      }, () => chrome.runtime.lastError);
+      chrome.contextMenus.create({
+        title: '2 Spaces',
+        contexts: ['action'],
+        id: 'indentation:2',
+        type: 'radio',
+        parentId: 'indentation'
+      }, () => chrome.runtime.lastError);
+      chrome.contextMenus.create({
+        title: '3 Spaces',
+        contexts: ['action'],
+        id: 'indentation:3',
+        type: 'radio',
+        parentId: 'indentation'
+      }, () => chrome.runtime.lastError);
+      chrome.contextMenus.create({
+        title: '4 Spaces',
+        contexts: ['action'],
+        id: 'indentation:4',
+        type: 'radio',
+        parentId: 'indentation'
+      }, () => chrome.runtime.lastError);
       chrome.storage.local.get({
-        expandLevel: 2
+        expandLevel: 2,
+        indentation: 2
       }, prefs => {
         chrome.contextMenus.update('expandLevel:' + prefs.expandLevel, {
+          checked: true
+        });
+        chrome.contextMenus.update('indentation:' + prefs.indentation, {
           checked: true
         });
       });
@@ -222,31 +283,45 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       'expandLevel': Number(info.menuItemId.replace('expandLevel:', ''))
     });
   }
+  else if (info.menuItemId === 'indentation:\t') {
+    chrome.storage.local.set({
+      'indentation': '\t'
+    });
+  }
+  else if (info.menuItemId.startsWith('indentation:')) {
+    chrome.storage.local.set({
+      'indentation': Number(info.menuItemId.replace('indentation:', ''))
+    });
+  }
 });
 
 /* FAQs & Feedback */
 {
-  const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
+  chrome.management = chrome.management || {
+    getSelf(c) {
+      c({installType: 'normal'});
+    }
+  };
   if (navigator.webdriver !== true) {
-    const {homepage_url: page, name, version} = getManifest();
-    onInstalled.addListener(({reason, previousVersion}) => {
-      management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
+    const {homepage_url: page, name, version} = chrome.runtime.getManifest();
+    chrome.runtime.onInstalled.addListener(({reason, previousVersion}) => {
+      chrome.management.getSelf(({installType}) => installType === 'normal' && chrome.storage.local.get({
         'faqs': true,
         'last-update': 0
       }, prefs => {
         if (reason === 'install' || (prefs.faqs && reason === 'update')) {
           const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
           if (doUpdate && previousVersion !== version) {
-            tabs.query({active: true, lastFocusedWindow: true}, tbs => tabs.create({
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, tbs => chrome.tabs.create({
               url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
               active: reason === 'install',
               ...(tbs && tbs.length && {index: tbs[0].index + 1})
             }));
-            storage.local.set({'last-update': Date.now()});
+            chrome.storage.local.set({'last-update': Date.now()});
           }
         }
       }));
     });
-    setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+    chrome.runtime.setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
   }
 }
